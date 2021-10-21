74b43276cda3d167979bd59e653f45150de0aa54
Piotr Maj <piotrmaj17@gmail.com>
2021-10-21 21:13:16 +0200

    UsersController cleanup

--- Users.Api/Controllers/UsersController.cs
+++ Users.Api/Controllers/UsersController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -10,33 +10,14 @@ namespace Users.Api.Controllers
     public class UsersController : Controller
     {
         private readonly ILogger<UsersController> logger;
-        public static readonly string Something = "Value";
 
-        public UsersController(ILogger<UsersController> logger) => this.logger = logger;
+        public UsersController(ILogger<UsersController> logger)
+        {
+            this.logger = logger;
+        }
 
         [HttpGet]
         [Route("/")]
-        public IEnumerable<string> Get()
-        {
-            var users = new List<string>();
-            var test = "string";
-            if (1 == 1)
-            {
-                users.Add(Something);
-            }
-            else
-            {
-            }
-
-            var variable2 = "test";
-            users.Add(variable2);
-            users.Add(test);
-            users.Add(Something);
-            users.Add(string.Concat(variable2, test));
-            Console.WriteLine("asdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdasasdadasdasdasdasdas");
-            this.logger.LogTrace("UsersController::Get was called");
-
-            return users;
-        }
+        public IEnumerable<string> Get() => Array.Empty<string>();
     }
 }
